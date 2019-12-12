a8c442826b297444cdfbbdcfb313b2435fc2ac79
SaundersCox <saunderscox@gmail.com>
2019-12-12 16:41:15 -0500

    Updated About, update image switcher

---
+++ changeImage.js
@@ -0,0 +1,13 @@
+function changeImage() {
+	var str = document.getElementById("imgClickAndChange").src;
+	var res = str.substring(str.length - 5, str.length - 4);
+	
+	if (res == 5) {
+		document.getElementById("imgClickAndChange").src = "me0.jpg";
+	}
+	else {
+		var next = str.substring(0, str.length - 7);
+		next += "me" + (parseInt(res)+1) + ".jpg"
+		document.getElementById("imgClickAndChange").src = next;
+	}
+}
