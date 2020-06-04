5f0538e610f232b4246c6eeeeef8f04bca5e1c55
SaundersCox <saunderscox@gmail.com>
2020-06-04 00:27:07 -0400

    reorganized furniture, added too many colors, added font added annoying lightening shadows

--- scripts/randomImage.js
+++ scripts/randomImage.js
@@ -5,5 +5,4 @@ randomImage()
 function randomImage() {
     n = Math.floor(Math.random() * NUM_IMAGES);
     document.getElementById("imgClickAndChange").src = "me" + n + ".jpg";
-    console.log(n) 
 }
