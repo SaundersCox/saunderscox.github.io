adf3805dab7da1362e9767da42d4a5e76bce38a1
saunderscox <saunderscox@gmail.com>
2020-11-19 02:08:51 -0500

    added setup.js

---
+++ cs4644/setup.js
@@ -0,0 +1,171 @@
+
+var drill = {
+  credit: "The Dev Team",
+  name: "Test"
+};
+
+// Initial variables
+var numPerformers = 0;
+var curSet = 0;
+var numSets = 1;
+// Instruments: Trumpet, Trombone, Tuba, Clarinet, Alto Sex, Tenor Sax, Piccolo, Snare, Bass, Cymbal
+
+
+
+// Map of performer name to ID
+var performerMapSample = {};
+performerMapSample["Alice"] = 0;
+
+// All performer info
+var performersSample = [
+  {
+    id: 0,
+    name: "Alice",
+    inst: "Piccolo",
+    sets: [
+      {
+        x: 20,
+        y: 20,
+      }
+      , {
+        x: 15,
+        y: 15
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: "Brian",
+    inst: "Clarinet",
+    sets: [
+      {
+        x: 30,
+        y: 20,
+      }
+      , {
+        x: 30,
+        y: 10
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Chris",
+    inst: "Alto Sax",
+    sets: [
+      {
+        x: 40,
+        y: 20,
+      }
+      , {
+        x: 45,
+        y: 15
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "David",
+    inst: "Tenor Sax",
+    sets: [
+      {
+        x: 20,
+        y: 30,
+      }
+      , {
+        x: 10,
+        y: 30
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "Erin",
+    inst: "Trumpet",
+    sets: [
+      {
+        x: 30,
+        y: 30,
+      }
+      , {
+        x: 30,
+        y: 30
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: "Faith",
+    inst: "Trombone",
+    sets: [
+      {
+        x: 40,
+        y: 30,
+      }
+      , {
+        x: 50,
+        y: 30
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: "George",
+    inst: "Tuba",
+    sets: [
+      {
+        x: 20,
+        y: 40,
+      }
+      , {
+        x: 15,
+        y: 45
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: "Harris",
+    inst: "Bass Drum",
+    sets: [
+      {
+        x: 30,
+        y: 40,
+      }
+      , {
+        x: 30,
+        y: 50
+      }
+    ]
+  },
+  {
+    id: 8,
+    name: "Irene",
+    inst: "Snare",
+    sets: [
+      {
+        x: 40,
+        y: 40,
+      }
+      , {
+        x: 45,
+        y: 45
+      }
+    ]
+  },
+  {
+    id: 9,
+    name: "John",
+    inst: "Cymbal",
+    sets: [
+      {
+        x: 30,
+        y: 50,
+      }
+      , {
+        x: 30,
+        y: 60
+      }
+    ]
+  }
+]
